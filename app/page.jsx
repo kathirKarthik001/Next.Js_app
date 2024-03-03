311aa24817f9da916505f7e66303ee1232b915f4
kathirKarthik001 <kathirkarthik001@gmail.com>
2024-03-03 23:02:33 +0530

    search bar and api added

--- app/page.jsx
+++ app/page.jsx
@@ -1,17 +1,35 @@
-import React from 'react'
-import Link from 'next/link'
+'use client';
+import { useState, useEffect } from 'react';
+import Link from 'next/link';
+import LoadingPage from './loading';
+import Courses from './components/Courses';
+import CourseSearch from './components/CourseSearch';
 
 const HomePage = () => {
+  const [courses, setCourses] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchCourses = async () => {
+      const res = await fetch('/api/courses');
+      const data = await res.json();
+      setCourses(data);
+      setLoading(false);
+    };
+
+    fetchCourses();
+  }, []);
+
+  if (loading) {
+    return <LoadingPage />;
+  }
+
   return (
     <>
-    <h1>Welcome to the kathir's media</h1>
-    <ul>
-      <li><Link href='/'>Home</Link></li>
-      <li><Link href='/about'>About</Link></li>
-      <li><Link href='/about/team' >Team</Link></li>
-    </ul>
+      <h1>Welcome To Traversy Media</h1>
+      <CourseSearch getSearchResults={(results) => setCourses(results)} />
+      <Courses courses={courses} />
     </>
-  )
-}
-
-export default HomePage
+  );
+};
+export default HomePage;
